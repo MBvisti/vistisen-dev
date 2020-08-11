5d7ff797e5bad3c9b0f17aebbea1f3e0050e7289
Morten Vistisen <mv@legalmonster.com>
2020-08-11 19:27:32 +0200

    meta

---
+++ screens/components/meta/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+const Meta = () => (
+    <Head>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta charSet='utf-8' />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,600,700" />
+        <title>vistisen</title>
+    </Head>
+);
+
+export default Meta;
