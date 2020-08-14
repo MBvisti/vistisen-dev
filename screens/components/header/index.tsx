652a653b1f74224d9cb69daa5b27aac61515884e
Morten Vistisen <mv@legalmonster.com>
2020-08-14 16:36:59 +0200

    fixed icon import

--- screens/components/header/index.tsx
+++ screens/components/header/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-// import Link from "next/link";
-import {ThreeBars} from "@styled-icons/octicons/ThreeBars/ThreeBars";
+import {ThreeBars} from "@styled-icons/octicons/ThreeBars";
 
 interface StyleProps {
     readonly fontWeight: number
