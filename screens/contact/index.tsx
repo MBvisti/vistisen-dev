452435a75ae40903411c5ef9c71657f7c286f811
Morten Vistisen <mv@legalmonster.com>
2020-08-17 10:40:48 +0200

    wip

--- screens/contact/index.tsx
+++ screens/contact/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Container = styled.div`
     display: flex;
     flex-direction: column;
-    min-height: 100%;
+    min-height: 35rem;
             
     h1 {
         color: ${props => props.theme.primary};
