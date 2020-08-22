e951af62c47758d79494db17f10538e87f250eb7
Morten Vistisen <mv@legalmonster.com>
2020-08-22 15:58:50 +0200

    work section 1.0 done

---
+++ pages/work.tsx
@@ -0,0 +1,7 @@
+import {Work} from "../screens/work";
+
+export default function WorkPage() {
+    return (
+        <Work />
+    )
+}
---
+++ screens/work/index.tsx
@@ -0,0 +1,148 @@
+import React from 'react';
+import styled from 'styled-components'
+import {TitleMedium, TitleSmall} from "../components/headings";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    
+    h2 {
+        margin-bottom: 0.7rem;
+    }
+    
+    p {
+        color: ${props => props.theme.primary };
+        font-size: 1.2rem;
+        font-weight: 300;
+    }
+`;
+
+export const ExperienceContainer = styled.div`
+    margin-top: 4rem;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+       
+    
+    overflow-y: scroll;
+`;
+
+export const Experience = styled.div`
+    display: flex;
+    width: 100%;
+    margin-bottom: 2rem;
+    cursor: pointer;
+`;
+
+export const ImageContainer = styled.div`
+    flex: 0 1 25%;
+    margin-right: 1rem;
+    display: flex;    
+    align-items: flex-start;
+    justify-content: flex-start;
+    height: 100%;
+
+    img {
+        max-width: 70%;
+        max-height: 70%;
+    }
+`;
+
+export const ShortDescContainer = styled.div`
+    flex: 1;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 0 1rem 1rem 0;              
+        
+    p {
+        color: ${props => props.theme.primary };
+        font-size: 1.25rem;
+        font-weight: 300;
+        margin-top: 0.3rem;
+        margin-bottom: 0.1rem;
+    }
+       
+`;
+
+export const Description = styled.div`
+    text-overflow: ellipsis;
+    overflow: hidden;
+    // white-space: nowrap;
+    color: white;
+    max-width: 20rem;
+`;
+
+
+export const Work = () => {
+    return (
+        <Container>
+            <TitleMedium marginTop={30}>
+                Work
+            </TitleMedium>
+            <p>
+                Below is a list of my work experiences. This includes everything from previous jobs, freelance work and side projects.
+            </p>
+            <ExperienceContainer>
+                <Experience>
+                    <ImageContainer>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png" alt="google logo" />
+                    </ImageContainer>
+                    <ShortDescContainer>
+                        <TitleSmall marginTop={0}>
+                            Legal Monster ApS
+                        </TitleSmall>
+                        <p>
+                            1/09/2019 to 1/10/2020
+                        </p>
+                        <p>
+                            Full-Stack developer
+                        </p>
+                        <p>
+                            Ruby on Rails - Typescript - React
+                        </p>
+                    </ShortDescContainer>
+                </Experience>
+                <Experience>
+                    <ImageContainer>
+                        <img src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png" alt="netflix logo" />
+                    </ImageContainer>
+                    <ShortDescContainer>
+                        <TitleSmall marginTop={0}>
+                            HS Moeller ApS
+                        </TitleSmall>
+                        <p>
+                            1/09/2019 to 1/10/2020
+                        </p>
+                        <p>
+                            It consultant
+                        </p>
+                        <p>
+                            React - Styled components - Figma
+                        </p>
+                    </ShortDescContainer>
+                </Experience>
+                <Experience>
+                    <ImageContainer>
+                        <img src="https://f0.pngfuel.com/png/497/515/facebook-scalable-graphics-icon-facebook-logo-facebook-logo-png-clip-art.png" alt="facebook logo" />
+                    </ImageContainer>
+                    <ShortDescContainer>
+                        <TitleSmall marginTop={0}>
+                            Easy Skat ApS
+                        </TitleSmall>
+                        <p>
+                            1/09/2019 to 1/10/2020
+                        </p>
+                        <p>
+                            Full-Stack developer
+                        </p>
+                        <p>
+                            React - Styled components - Figma
+                        </p>
+                    </ShortDescContainer>
+                </Experience>
+            </ExperienceContainer>
+        </Container>
+    )
+}
