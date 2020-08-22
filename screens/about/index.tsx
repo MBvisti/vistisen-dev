f9d51308eeeb26eca66a1ebb6f925061c802eaa8
Morten Vistisen <mv@legalmonster.com>
2020-08-22 14:20:12 +0200

    Update index.tsx

--- screens/about/index.tsx
+++ screens/about/index.tsx
@@ -1,40 +1,22 @@
-import React, {useState} from "react";
+import React from "react";
 import styled from 'styled-components';
-
-interface StyleProps {
-    readonly selected: boolean;
-}
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
-    
+       
     h2 {
-        margin-top: 5rem;
+        margin-top: 30%;
         color: ${props => props.theme.primary};
         font-size: 2.1rem;
     }  
 `;
 
-const OptionsContainer = styled.div`
-    display: flex;
-    margin-top: 1rem;
-    cursor: pointer;    
-`
-
-const Option = styled.p<StyleProps>`
-    color: ${props => props.selected ? props.theme.secondary : props.theme.primary};
-    font-size: 1.2rem;
-    font-weight: 300;
-    margin-right: 2rem;
-`
-
 const TextContainer = styled.div`
     display: flex;
     flex-direction: column;
     margin-top: 2.5rem;
-    height: 100%;
-    line-height: 1.4;
+    line-height: 1.3;
     
     p {
         text-align: justify;
@@ -43,49 +25,30 @@ const TextContainer = styled.div`
         margin-bottom: 1rem;
     }
 `
-const ResumeContainer = styled.div`
-    margin-top: 2.5rem;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    color: ${props => props.theme.primary};
-`;
 
+// TODO: this should be really simply and concise. Who I am, my preferred technology, what I want to do and what I have done work and education.
 export const About = () => {
-    const [state, setState] = useState("me")
     return (
         <Container>
             <h2>
-                About
+                About me
             </h2>
-            <OptionsContainer>
-                <Option selected={state === "me" ? true : false} onClick={() => setState('me')}>Me</Option>
-                <Option selected={state === "resume" ? true : false} onClick={() => setState('resume')}>Resume</Option>
-            </OptionsContainer>
-            {
-                state === 'me' ?
-                <TextContainer>
-                    <p>
-                        Originally from Denmark, I'm currently living in Berlin, Germany working as a freelance full-stack developer. I have
-
-                    </p>
-                    <p>
-                        My educational background is in business where I have a master in finance and international
-                        business.
-                    </p>
-                    <p>
-                        I mainly work with Javascript/Typescript, React, Golang and PostgreSQL. However, I do also have working
-                        experience with Ruby on Rails, Node.js and Python.
-                    </p>
-                    <p>
-                        I'm open to work on a freelance or contracting basis.
-                    </p>
-                </TextContainer>
-                    :
-                <ResumeContainer>
-                    <p>MAAAARDEN's Resume</p>
-                </ResumeContainer>
-            }
+            <TextContainer>
+                <p>
+                    Originally from Denmark, I'm currently living in Berlin, Germany working as a freelance full-stack developer. I have
+                </p>
+                <p>
+                    My educational background is in business where I have a master in finance and international
+                    business.
+                </p>
+                <p>
+                    I mainly work with Javascript/Typescript, React, Golang and PostgreSQL. However, I do also have working
+                    experience with Ruby on Rails, Node.js and Python.
+                </p>
+                <p>
+                    I'm open to work on a freelance or contracting basis.
+                </p>
+            </TextContainer>
         </Container>
     )
 }
