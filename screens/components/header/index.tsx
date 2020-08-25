0864c88e566f971bfb180f0829c0d207e54e75c6
Morten Vistisen <mv@legalmonster.com>
2020-08-25 20:02:38 +0200

    added navigation

--- screens/components/header/index.tsx
+++ screens/components/header/index.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import { useRouter } from 'next/router'
 import styled from "styled-components";
 import {ThreeBars} from "@styled-icons/octicons/ThreeBars";
+import {CloseCircle} from "@styled-icons/ionicons-outline/CloseCircle";
 import Link from "next/link";
+import {TitleMedium} from "../headings";
+
 
 interface StyleProps {
     readonly fontWeight: number
@@ -12,6 +15,7 @@ const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  position: relative; 
   
   p {
     color: ${props => props.theme.primary};    
@@ -36,18 +40,74 @@ const Nav = styled.nav`
     height: 2rem;
     cursor: pointer;    
   }
+  
+  @media (min-width: 640px) {
+    svg {
+        display: none;
+    }
+  }
 `
 
-// const AnchorElement = styled.a`
-//     color: ${props => props.theme.darkColor};
-//     font-weight: 500;
-//     cursor: pointer;
-//     font-size: 1.2rem;
-// `;
+const LargeScreenNav = styled.div`
+  display: none;
+  width: 8rem;
+  justify-content: space-between;
+  
+  @media (min-width: 640px) {
+    display: flex;
+  }  
+`;
 
+const AnchorElement = styled.a`
+    color: ${props => props.theme.secondary};
+    font-weight: 500;
+    cursor: pointer;
+    font-size: 1.95rem;
+`;
+
+const NavModal = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    top: 0;
+    background: ${props => props.theme.primary};
+    padding: 2.5rem;
+    
+    svg {
+        color: ${ props => props.theme.accentTwo};
+        width: 3rem;
+        height: 3rem;
+        right: 0;
+        position: absolute;
+        margin-right: 20px;
+        cursor: pointer;    
+    }
+    
+    h2 {
+        color: ${props => props.theme.accentTwo};
+        margin-bottom: 2rem;
+    }
+    
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 13rem;
+    }
+        
+    @media (min-width: 640px) {
+      display: none;
+    }
+    
+`;
 
 const Header = () => {
+    const [state, setState] = useState(false)
     const {pathname} = useRouter()
+    console.log(state);
     return (
         <Container>
             <Logo>
@@ -63,14 +123,45 @@ const Header = () => {
                 </p>
             }
             <Nav>
-                <ThreeBars/>
-                {/*<Link href="/about">*/}
-                {/*    <AnchorElement>About</AnchorElement>*/}
-                {/*</Link>*/}
-                {/*<Link href="/blog">*/}
-                {/*    <AnchorElement>Blog</AnchorElement>*/}
-                {/*</Link>*/}
+                {
+                    !state &&
+                    <ThreeBars onClick={() => setState(!state)}/>
+                }
+                <LargeScreenNav>
+                    <Link href="/about">
+                        <AnchorElement>About</AnchorElement>
+                    </Link>
+                    <Link href="/blog">
+                        <AnchorElement>Blog</AnchorElement>
+                    </Link>
+                </LargeScreenNav>
             </Nav>
+            {
+                state &&
+                <NavModal>
+                    <CloseCircle onClick={() => setState(!state)}/>
+                        <TitleMedium>
+                            Navigation
+                        </TitleMedium>
+                    <div>
+                        <Link href="/">
+                            <AnchorElement onClick={() => setState(false)}>Home</AnchorElement>
+                        </Link>
+                        <Link href="/work">
+                            <AnchorElement onClick={() => setState(false)}>Work</AnchorElement>
+                        </Link>
+                        <Link href="/contact">
+                            <AnchorElement onClick={() => setState(false)}>Contact</AnchorElement>
+                        </Link>
+                        {/*<Link href="/about">*/}
+                        {/*    <AnchorElement onClick={() => setState(false)}>About</AnchorElement>*/}
+                        {/*</Link>*/}
+                    </div>
+                    {/*<Link href="/blog">*/}
+                    {/*    <AnchorElement>Blog</AnchorElement>*/}
+                    {/*</Link>*/}
+                </NavModal>
+            }
         </Container>
     );
 }
