7cf853e61afce69e25bc52e6700284ffba9eff9a
Morten Vistisen <mv@legalmonster.com>
2020-08-25 20:02:33 +0200

    heading comps
    need color options

--- screens/components/headings/index.tsx
+++ screens/components/headings/index.tsx
@@ -1,23 +1,35 @@
 import styled from 'styled-components';
 
 interface StyleProps {
-    readonly marginTop: number;
+    readonly marginTop?: number;
+    readonly marginRight?: number;
+    readonly marginBottom?: number;
+    readonly marginLeft?: number;
 }
 
 export const TitleBig = styled.h1<StyleProps>`
     margin-top: ${props => props.marginTop}%;
+    margin-Right: ${props => props.marginRight}%;
+    margin-bottom: ${props => props.marginBottom}%;
+    margin-left: ${props => props.marginLeft}%;  
     color: ${props => props.theme.primary};
     font-size: 2.4rem;
 `;
 
 export const TitleMedium = styled.h2<StyleProps>`
     margin-top: ${props => props.marginTop}%;
+    margin-Right: ${props => props.marginRight}%;
+    margin-bottom: ${props => props.marginBottom}%;
+    margin-left: ${props => props.marginLeft}%;
     color: ${props => props.theme.primary};
     font-size: 1.95rem;
 `;
 
 export const TitleSmall = styled.h3<StyleProps>`
     margin-top: ${props => props.marginTop}%;
+    margin-Right: ${props => props.marginRight}%;
+    margin-bottom: ${props => props.marginBottom}%;
+    margin-left: ${props => props.marginLeft}%;
     color: ${props => props.theme.primary};
     font-size: 1.563rem;
 `;
