c74951e83170f219ed048e0d4c89ba530e04b33a
Morten Vistisen <mv@legalmonster.com>
2020-09-09 19:37:01 +0200

    Update index.tsx

--- screens/components/meta/index.tsx
+++ screens/components/meta/index.tsx
@@ -2,10 +2,17 @@ import Head from 'next/head';
 
 const Meta = () => (
     <Head>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta charSet='utf-8' />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:thin,normal,bold&family=Roboto:thin,normal,bold" />
-        <title>vistisen</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+        <meta
+            name="Description"
+            content="Developer portfolio for Morten Vistisen"
+        ></meta>
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Montserrat:thin,normal,bold&family=Roboto:thin,normal,bold"
+        />
+        <title>Vistisen</title>
     </Head>
 );
 
