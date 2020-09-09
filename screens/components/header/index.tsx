99b9c6d7cf0ca5bb6275c612f530feb10711c7c1
Morten Vistisen <mv@legalmonster.com>
2020-09-09 19:29:34 +0200

    Update index.tsx

--- screens/components/header/index.tsx
+++ screens/components/header/index.tsx
@@ -1,76 +1,75 @@
-import React, {useState} from "react";
-import { useRouter } from 'next/router'
-import styled from "styled-components";
-import {ThreeBars} from "@styled-icons/octicons/ThreeBars";
-import {CloseCircle} from "@styled-icons/ionicons-outline/CloseCircle";
-import Link from "next/link";
-import {TitleMedium} from "../headings";
-
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import { ThreeBars } from '@styled-icons/octicons/ThreeBars';
+import { CloseCircle } from '@styled-icons/ionicons-outline/CloseCircle';
+import Link from 'next/link';
+import { TitleMedium } from '../headings';
 
 interface StyleProps {
-    readonly fontWeight: number
+    readonly fontWeight: number;
 }
 
 const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative; 
-  
-  p {
-    color: ${props => props.theme.primary};    
-  }
-`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+
+    p {
+        color: ${(props) => props.theme.primary};
+    }
+`;
 
 const Logo = styled.div`
-  display: flex;
-  flex-direction: column;
-  cursor: pointer;
-`
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+`;
 const Paragraph = styled.p<StyleProps>`
     color: ${({ theme }) => theme.primary};
     font-size: 1.4rem;
-    font-weight: ${props => props.fontWeight};
+    font-weight: ${(props) => props.fontWeight};
 `;
 
-const Nav = styled.nav`      
-  svg {
-    color: ${({ theme }) => theme.primary};
-    width: 2rem;
-    height: 2rem;
-    cursor: pointer;    
-  }
-  
-  @media (min-width: 640px) {
+const Nav = styled.nav`
     svg {
-        display: none;
+        color: ${({ theme }) => theme.primary};
+        width: 2rem;
+        height: 2rem;
+        cursor: pointer;
     }
-  }
-`
+
+    @media (min-width: 640px) {
+        svg {
+            display: none;
+        }
+    }
+`;
 
 const LargeScreenNav = styled.div`
-  display: none;
-  width: 14rem;
-  justify-content: space-between;  
-  
-  @media (min-width: 640px) {
-    display: flex;
-  }  
+    display: none;
+    justify-content: space-between;
+
+    @media (min-width: 640px) {
+        display: flex;
+    }
 `;
 
 const AnchorElementSmall = styled.a`
-    color: ${props => props.theme.secondary};
+    color: ${(props) => props.theme.secondary};
     font-weight: 500;
     cursor: pointer;
+    margin: 0 1rem 0 1rem;
     font-size: 1.2rem;
-    
+
     :hover {
-      color: ${props => props.theme.primary};    
+        color: ${(props) => props.theme.primary};
     }
 `;
 
 const AnchorElementBig = styled.a`
-    color: ${props => props.theme.secondary};
+    color: ${(props) => props.theme.secondary};
     font-weight: 500;
     cursor: pointer;
     font-size: 1.95rem;
@@ -84,84 +83,68 @@ const NavModal = styled.div`
     left: 0;
     bottom: 0;
     top: 0;
-    background: ${props => props.theme.primary};
+    background: ${(props) => props.theme.primary};
     padding: 2.5rem;
     z-index: 500;
-    
+
     svg {
-        color: ${ props => props.theme.accentTwo};
+        color: ${(props) => props.theme.accentTwo};
         width: 3rem;
         height: 3rem;
         right: 0;
         position: absolute;
         margin-right: 20px;
-        cursor: pointer;    
+        cursor: pointer;
     }
-    
+
     h2 {
-        color: ${props => props.theme.accentTwo};
+        color: ${(props) => props.theme.accentTwo};
         margin-bottom: 2rem;
     }
-    
+
     div {
         display: flex;
         flex-direction: column;
         justify-content: space-between;
         height: 13rem;
-        
+
         a {
-            color: ${props => props.theme.accentOne};
+            color: ${(props) => props.theme.accentOne};
         }
     }
-        
+
     @media (min-width: 640px) {
-      display: none;
+        display: none;
     }
 `;
 
 const BreadcrumbNavigation = styled.div`
     display: flex;
-    
+
     p {
         margin-right: 0.4rem;
-        color: ${props => props.theme.secondary};
+        color: ${(props) => props.theme.secondary};
         font-weight: 500;
-        font-family: 'Montserrat',sans-serif;
+        font-family: 'Montserrat', sans-serif;
         font-size: 1.2rem;
     }
 `;
 
 const Header = () => {
     const [state, setState] = useState(false);
-    const {asPath, } = useRouter();
-    const pathArray = asPath.split("/").slice(1);
+    const { asPath } = useRouter();
+    const pathArray = asPath.split('/').slice(1);
 
     return (
         <Container>
             <Logo>
                 <Link href="/">
-                    <Paragraph fontWeight={600}>
-                        vis-ti-sen
-                    </Paragraph>
+                    <Paragraph fontWeight={600}>vis-ti-sen</Paragraph>
                 </Link>
             </Logo>
-            { asPath != "/" &&
-                <BreadcrumbNavigation>
-                    {
-                        pathArray.map(path => {
 
-                            return (
-                                <p>{path}/ </p>
-                            )
-                        })
-                    }
-                </BreadcrumbNavigation>
-            }
             <Nav>
-                {
-                    !state &&
-                    <ThreeBars onClick={() => setState(!state)}/>
-                }
+                {!state && <ThreeBars onClick={() => setState(!state)} />}
                 <LargeScreenNav>
                     <Link href="/">
                         <AnchorElementSmall>Home</AnchorElementSmall>
@@ -172,36 +155,44 @@ const Header = () => {
                     <Link href="/contact">
                         <AnchorElementSmall>Contact</AnchorElementSmall>
                     </Link>
+                    <Link href="/about">
+                        <AnchorElementSmall>About</AnchorElementSmall>
+                    </Link>
                 </LargeScreenNav>
             </Nav>
-            {
-                state &&
+            {state && (
                 <NavModal>
-                    <CloseCircle onClick={() => setState(!state)}/>
-                        <TitleMedium>
-                            Navigation
-                        </TitleMedium>
+                    <CloseCircle onClick={() => setState(!state)} />
+                    <TitleMedium>Navigation</TitleMedium>
                     <div>
                         <Link href="/">
-                            <AnchorElementBig onClick={() => setState(false)}>Home</AnchorElementBig>
+                            <AnchorElementBig onClick={() => setState(false)}>
+                                Home
+                            </AnchorElementBig>
                         </Link>
                         <Link href="/work">
-                            <AnchorElementBig onClick={() => setState(false)}>Work</AnchorElementBig>
+                            <AnchorElementBig onClick={() => setState(false)}>
+                                Work
+                            </AnchorElementBig>
                         </Link>
                         <Link href="/contact">
-                            <AnchorElementBig onClick={() => setState(false)}>Contact</AnchorElementBig>
+                            <AnchorElementBig onClick={() => setState(false)}>
+                                Contact
+                            </AnchorElementBig>
                         </Link>
-                        {/*<Link href="/about">*/}
-                        {/*    <AnchorElement onClick={() => setState(false)}>About</AnchorElement>*/}
-                        {/*</Link>*/}
+                        <Link href="/about">
+                            <AnchorElementBig onClick={() => setState(false)}>
+                                About
+                            </AnchorElementBig>
+                        </Link>
                     </div>
                     {/*<Link href="/blog">*/}
                     {/*    <AnchorElement>Blog</AnchorElement>*/}
                     {/*</Link>*/}
                 </NavModal>
-            }
+            )}
         </Container>
     );
-}
+};
 
 export default Header;
