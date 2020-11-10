01fb16876db9b151fa3d1a55dcc45d1170777977
Morten Vistisen <mv@legalmonster.com>
2020-11-10 18:27:20 +0100

    minor touches

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Docker, ReactLogo } from '@styled-icons/fa-brands';
-import { Javascript, Postgresql, Typescript } from '@styled-icons/simple-icons';
 import Link from 'next/link';
 
 const IntroductionContainer = styled.div`
@@ -15,21 +13,14 @@ const IntroductionContainer = styled.div`
 
     button:hover {
         background: ${(props) => props.theme.primary};
-        border: 1px solid ${(props) => props.theme.accentTwo};
+        border: 1px solid ${(props) => props.theme.secondary};
 
-        a {
-            color: ${(props) => props.theme.accentTwo};
-        }
+        color: ${(props) => props.theme.accentTwo};
     }
 
     @media (min-width: 640px) {
         margin: 0 auto;
-    }
-
-    @media (min-width: 1024px) {
-        margin: 0 auto;
         position: relative;
-
         img {
             display: inline-block;
             visibility: visible;
@@ -46,15 +37,24 @@ const Introduction = styled.div`
     display: flex;
     flex-direction: column;
     margin-bottom: 1rem;
+    padding-right: 5rem;
+
+    @media (min-width: 414px) {
+        padding-right: 8rem;
+    }
+
+    @media (min-width: 640px) {
+        padding-right: 0;
+    }
 `;
 
 const HeaderOne = styled.h1`
-    font-weight: 500;
+    font-weight: 700;
     color: ${(props) => props.theme.primary};
-    font-size: 4rem;
+    font-size: 3rem;
 
     @media (min-width: 1024px) {
-        font-size: 6.2rem;
+        font-size: 3.7rem;
     }
 `;
 
@@ -64,53 +64,29 @@ const HeaderTwo = styled.h2`
     font-size: 3rem;
 
     @media (min-width: 1024px) {
-        font-size: 5.1rem;
+        font-size: 3.1rem;
     }
 `;
 
-const StackIcons = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 90%;
-    margin-bottom: 3rem;
+interface ButtonProps {
+    readonly inverse?: boolean;
+}
 
-    svg {
-        height: 1.3rem;
-        width: 1.3rem;
-        cursor: pointer;
-        color: ${(props) => props.theme.secondary};
-    }
-
-    svg:hover {
-        fill: ${(props) => props.theme.primary};
-    }
-
-    @media (min-width: 1024px) {
-        width: 100%;
-        justify-content: space-around;
-
-        svg {
-            height: 1.5rem;
-            width: 1.5rem;
-            cursor: pointer;
-            color: ${(props) => props.theme.secondary};
-        }
-    }
-`;
-
-const Button = styled.button`
+const Button = styled.button<ButtonProps>`
     margin-top: 1.5rem;
-    width: 8rem;
-    height: 3.5rem;
-    background: ${(props) => props.theme.bg};
+    width: 9rem;
+    height: 4.5rem;
+    background: ${(props) =>
+        props.inverse ? props.theme.bg : props.theme.secondary};
     border: 1px solid ${(props) => props.theme.secondary};
     border-radius: 0.4rem;
     font-family: 'Roboto', sans-serif;
     cursor: pointer;
 
-    a {
-        color: ${(props) => props.theme.primary};
-    }
+    color: ${(props) =>
+        props.inverse ? props.theme.secondary : props.theme.bg};
+    font-size: 1.6rem;
+    font-size: bold;
 
     @media (min-width: 1024px) {
         width: 11rem;
@@ -118,24 +94,36 @@ const Button = styled.button`
     }
 `;
 
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 85%;
+
+    @media (min-width: 375px) {
+        width: 65%;
+    }
+
+    @media (min-width: 640px) {
+        width: 45%;
+    }
+`;
+
 export const Home = () => {
     return (
         <IntroductionContainer>
             <Introduction>
-                <HeaderTwo>Hi,</HeaderTwo>
-                <HeaderOne>I am Morten.</HeaderOne>
-                <HeaderTwo>Full Stack developer</HeaderTwo>
+                <HeaderTwo>Hi, I'm Morten.</HeaderTwo>
+                <HeaderOne>A freelance full-stack developer</HeaderOne>
+                <HeaderTwo>building cloud-native applications.</HeaderTwo>
             </Introduction>
-            <StackIcons>
-                <ReactLogo />
-                <Javascript />
-                <Typescript />
-                <Postgresql />
-                <Docker />
-            </StackIcons>
-            <Button>
-                <Link href="/contact">Say hello</Link>
-            </Button>
+            <ButtonContainer>
+                <Link href="/contact">
+                    <Button>Say hello</Button>
+                </Link>
+                <Link href="/work">
+                    <Button inverse={true}>My Work</Button>
+                </Link>
+            </ButtonContainer>
             <img
                 src="https://res.cloudinary.com/diwu3yx6a/image/upload/v1598441374/mig_selv-removebg-preview.png"
                 alt="maaaaaaaaaarden"
