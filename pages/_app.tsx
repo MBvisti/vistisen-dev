bc73f35b404208d647d2a5c1f4e0c99dc32128f6
mbv <heymbv@gmail.com>
2021-03-03 21:05:24 +0100

    removing global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,60 +1,9 @@
+import '../styles/globals.css'
 import React from 'react';
 import { AppProps } from 'next/app';
-import '../styles/globals.css'
 import { Layout } from '../screens/layout';
-import { createGlobalStyle } from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import Meta from '../screens/components/meta';
-
-export const GlobalStyle = createGlobalStyle`
-  html {
-    box-sizing: border-box;
-    font-size: 10px;
-    font-family: 'Montserrat', sans-serif;
-  }
-
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
-
-  body {
-    padding: 0;
-    margin: 0;
-    font-size: 1.5rem;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  fieldset {
-    border: none;
-    padding: 0;
-    margin: 0;
-  }
-
-  h1, h2, h3, h4 {
-    margin: 0;
-  }
-
-  p {
-    margin: 0;
-    font-family: 'Roboto', sans-serif;
-    color: #0A0C0D;
-  }
-
-  input:focus {
-    outline: none !important;
-  }
-`;
-
-// const darkTheme = {
-//     bg: "#0A0C0D",
-//     primary: "#F2F2F2",
-//     secondary: "#8C8C8C",
-//     accentOne: "#585959",
-//     accentTwo: "#252626",
-// }
 
 // should probably have accentDark and accentLight
 const lightTheme = {
@@ -70,7 +19,6 @@ function App({ Component, pageProps }: AppProps) {
 
     return (
         <ThemeProvider theme={theme}>
-            <GlobalStyle />
             <Meta />
             <Layout>
                 <Component {...pageProps} />
