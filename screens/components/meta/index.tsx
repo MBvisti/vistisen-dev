60dc8a703b5bc559ddbc8c81600934ed423c347e
mbv <heymbv@gmail.com>
2021-04-09 11:10:09 +0200

    removed tracking and updated title

--- screens/components/meta/index.tsx
+++ screens/components/meta/index.tsx
@@ -8,17 +8,10 @@ export const Meta = () => (
             name="Description"
             content="Developer portfolio for Morten Vistisen"
         ></meta>
-        <link rel="icon" href="https://example.com/favicon.png" />
         <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Montserrat:thin,normal,bold&family=Roboto:thin,normal,bold"
         />
-        <script
-            async
-            defer
-            data-website-id="989a3cf5-6f3c-4a09-b121-e9beae05c4c6"
-            src="https://mbv-websites-analytics.herokuapp.com/umami.js"
-        ></script>
-        <title>Vistisen</title>
+        <title>Morten Vistisen</title>
     </Head>
 );
