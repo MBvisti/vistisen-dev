c236c01332e8ef1413ef85101ff5e1da8ae47633
mbv <heymbv@gmail.com>
2021-04-10 19:46:02 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
     './pages/**/*.{js,jsx,ts,tsx}',
-    './screens/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
